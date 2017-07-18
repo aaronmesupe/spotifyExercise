20e020afa9ee54ca234ff46ba94d0a4472bc9305
Reyes Antequera <rantmed87@gmail.com>
2017-07-18 18:38:17 +0200

    error 401

--- authorization_code/public/spotify.js
+++ authorization_code/public/spotify.js
@@ -1,27 +1,25 @@
 (function($) {
 
-  var $inputValue = $('.searchArtist');
-  console
+
+  var $inputValue = $('.searchArtist'); 
   var $button = $('.search');
   var $divResults = $('.results');
-  var $client_id = 'acc91e66711c4f1c983ca6ffb9d96569';
-  var $client_secret = 'b5a571035c7c4db0988293c7a5c853f5';
+
 
   $button.on('click', function() {
-    //send the request and get the response
-    $divResults.get('https://api.spotify.com/v1/artists/0OdUWJ0sBjDrqHygGUXeCF', completeFunction;
+    $divResults.load('https://api.spotify.com/v1/search?type=artist&query=0TnOYISbd1XYRBk9myaseg', completeFunction);
     /*$divResults.get('https://api.spotify.com/v1/search?type=artist&query='+$inputValue, {}, completeFunction);*/
   });
   
-  //complete Function
+
   function completeFunction(responseText, textStatus, request) {
-    //add border 
-     $divResults.css('border', '1px solid #e8e8e8');
-    //check
+    
+     $divResults.css('border', '1px solid #000');
+   
     console.log(request);
-    //check errors
+   
     if(textStatus === 'error') {
-      //show me an massage
+   
       $divResults.text('Error del GÚENOOORL ' + request.status + ' ' + request.statusText);
     } 
   }
